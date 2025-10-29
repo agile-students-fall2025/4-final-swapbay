177909665a5ab67e15ba90defbc72df8d0328c67
Amanuel-Nigussie <160407550+Amanuel-Nigussie@users.noreply.github.com>
2025-10-29 04:56:00 -0400

    add mock chat data

---
+++ front-end/src/utils/mockChats.js
@@ -0,0 +1,24 @@
+export const mockChats = [
+  {
+    id: 1,
+    user: 'hailemariam',
+    lastMessage: 'Sure, I can bring it tomorrow.',
+    time: '2 h ago',
+    messages: [
+      { sender: 'me', text: 'Is the bike still available?', time: '2 h ago' },
+      { sender: 'hailemariam', text: 'Yes! Want to swap?', time: '2 h ago' },
+      { sender: 'me', text: 'Sure, I can bring it tomorrow.', time: '2 h ago' },
+    ],
+  },
+  {
+    id: 2,
+    user: 'sebahadin',
+    lastMessage: 'Great! see you at campus gate.',
+    time: '5 h ago',
+    messages: [
+      { sender: 'me', text: 'Camera lens still on sale?', time: '5 h ago' },
+      { sender: 'sebahadin', text: 'Yes, 50 mm one.', time: '5 h ago' },
+      { sender: 'me', text: 'Great! see you at campus gate.', time: '5 h ago' },
+    ],
+  },
+];
