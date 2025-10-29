58b8340cf6bc9745d6f84b012372f5ba56b483dd
Amanuel-Nigussie <160407550+Amanuel-Nigussie@users.noreply.github.com>
2025-10-29 03:11:31 -0400

    Set up basic initialization

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+  extend: {
+    keyframes: {
+      fadeIn: { '0%': { opacity: 0 }, '100%': { opacity: 1 } },
+    },
+    animation: { fadeIn: 'fadeIn .2s ease-in-out' },
+  },
+},
+  plugins: [],
+};
