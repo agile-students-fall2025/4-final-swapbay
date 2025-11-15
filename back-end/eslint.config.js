384680df708b6144515fd1372a1d58fa5373485b
Amanuel-Nigussie <160407550+Amanuel-Nigussie@users.noreply.github.com>
2025-11-15 17:48:39 -0500

    eslint

---
+++ back-end/eslint.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
+import { defineConfig } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default defineConfig([
+  ...compat.config({ extends: ['airbnb-base'] }),
+  {
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      'consistent-return': 'off',
+      'no-param-reassign': 'off',
+      'object-curly-newline': 'off',
+      'max-len': ['error', { code: 120, ignoreUrls: true }],
+      'no-console': 'off',
+    },
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
+]);
