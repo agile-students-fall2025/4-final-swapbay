cb669f1d8489242cbc8d6eebfc6b56eecc5f931f
sebahadin <167858194+sebahadin@users.noreply.github.com>
2025-12-10 09:50:19 +0100

    Disable eslint rule for component exports

--- front-end/src/context/ItemContext.jsx
+++ front-end/src/context/ItemContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import { api } from '../utils/api';
 import { useAuth } from './AuthContext';
